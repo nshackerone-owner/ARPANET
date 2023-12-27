290f1b68538bfa6abc4a85ec137e8516db1e2abc
DensusHere <134015788+DensusHere@users.noreply.github.com>
2023-12-27 14:30:06 +0300

    Update one_test.rs

--- tests/one_test.rs
+++ tests/one_test.rs
@@ -1,1 +1,26 @@
+use regex::Regex;
+fn main() {
+}
 
+#[test]
+
+fn test_bash_script() {
+    let content = r#"
+        #!/bin/bash
+        echo "Hello, World!"
+    "#;
+
+    std::fs::write("test_script.sh", content).unwrap();
+
+    let output = Command::new("bash")
+        .arg("test_script.sh")
+        .output()
+        .expect("Failed to execute command");
+
+    let stdout = String::from_utf8_lossy(&output.stdout);
+    let stderr = String::from_utf8_lossy(&output.stderr);
+
+    let re = Regex::new(r"Hello, World!").unwrap();
+    assert!(re.is_match(&stdout), "Unexpected stdout: {}", stdout);
+    assert_eq!(stderr, "", "Unexpected stderr: {}", stderr);
+}
